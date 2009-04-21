b18c617445073c4deffbe45ac920b2aeb7778447
TAKANO Mitsuhiro <takano32@gmail.com>
2009-04-22 07:47:07 +0900

    add section03 directory

---
+++ stdhaskell/section03/expand.hs
@@ -0,0 +1,11 @@
+
+
+main = do cs <- getContents
+          putStr $ expand cs
+
+expand :: String -> String
+expand cs = map translate cs
+
+translate :: Char -> Char
+translate c = if c == '\t' then '@' else c
+
