861a3cdbb607f5009d36038f62e07fcc37334b85
TAKANO Mitsuhiro <takano32@gmail.com>
2009-05-10 02:21:40 +0900

    fold.hs: rev.1

--- stdhaskell/section07/fold.hs
+++ stdhaskell/section07/fold.hs
@@ -1,1 +1,15 @@
 
+
+main = do cs <- getContents
+          putStr . unlines $ map fold $ lines cs
+
+fold :: String -> String
+fold ss = join $ splitAt 60 ss
+    where
+      join (a, "")  = a
+      join (a, b)   = a ++ "\n" ++ fold b
+
+
+
+
+
